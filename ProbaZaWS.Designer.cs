ad5abf4f0b285776f2ed05f9002e6035986bcb44
Milanko Topolski <milanko.topolski@ict.edu.rs>
2016-11-11 21:12:23 +0100

    Postavljanje fajlova

---
+++ ProbaZaWS.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WSKorisnik
+{
+    partial class ProbaZaWS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(163, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(285, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(163, 67);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(285, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Izaberite kategoriju:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Postovi u izabranoj kategoriji:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(13, 93);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "XML",
+            "JSON"});
+            this.comboBox3.Location = new System.Drawing.Point(163, 13);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(285, 21);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Izaberite format odgovora:";
+            // 
+            // ProbaZaWS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 390);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "ProbaZaWS";
+            this.Text = "Proba za WS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+
+
+
+
+
+
+
+
+    }
+}
+
---
+++ ProbaZaWS.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WSKorisnik
+{
+    public partial class ProbaZaWS : Form
+    {
+        public ProbaZaWS()
+        {
+            InitializeComponent();
+        }
+
+        private string[] links = new string[50];
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            linkLabel1.Text = "";
+            int i = 0;
+            if (comboBox2.Size.IsEmpty)
+            {
+                comboBox2.Text = "";
+            }
+            else { comboBox2.Items.Clear(); comboBox2.Text = ""; }
+            WSProxies.WebService service = new WSProxies.WebService();
+            if (comboBox3.SelectedItem.ToString() == "XML")
+            {
+                WSProxies.Post[] postovi = service.getPosts(comboBox1.SelectedItem.ToString());
+                foreach (WSProxies.Post post in postovi)
+                {
+                    comboBox2.Items.Add(post.Naslov);
+                    links[i] = post.LinkKaPostu;
+                    i++;
+                }
+            }
+            else if (comboBox3.SelectedItem.ToString() == "JSON")
+            {
+                WSProxies.Post[] postoviJson = service.getPostsJson(comboBox1.SelectedItem.ToString());
+                foreach (WSProxies.Post post in postoviJson)
+                {
+                    comboBox2.Items.Add(post.Naslov);
+                    links[i] = post.LinkKaPostu;
+                    i++;
+                }
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {   
+            linkLabel1.Text = links[comboBox2.SelectedIndex];
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(linkLabel1.Text);
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear(); 
+            comboBox1.Text = "";
+            comboBox2.Items.Clear();
+            comboBox2.Text = "";
+            linkLabel1.Text = "";
+            WSProxies.WebService service = new WSProxies.WebService();
+            if (comboBox3.SelectedItem.ToString() == "XML")
+            {
+                WSProxies.Kategorija[] kat = service.getKategorije();
+                foreach (WSProxies.Kategorija kate in kat)
+                {
+                    comboBox1.Items.Add(kate.nazivKategorije);
+                }
+            }
+            else if (comboBox3.SelectedItem.ToString() == "JSON")
+            {
+                WSProxies.Kategorija[] katJson = service.getKategorijeJson();
+                foreach (WSProxies.Kategorija kate in katJson)
+                {
+                    comboBox1.Items.Add(kate.nazivKategorije);
+                }
+            }
+        }
+    }
+}
